66cac2e1648ce0dcd82fec9ddedf549b4d3d7f23
tmayr@tmayr.com <tmayr@tmayr.com>
2019-10-10 14:50:43 -0300

    enable input tag name and build args

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,16 +32,19 @@ if [[ -z "$INPUT_BUILD_CONTEXT" ]]; then
 	exit 1
 fi
 
-
 # The following environment variables will be provided by the environment automatically: GITHUB_REPOSITORY, GITHUB_SHA
 
 # send credentials through stdin (it is more secure)
 echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin docker.pkg.github.com
 
 # Set Local Variables
-shortSHA=$(echo "${GITHUB_SHA}" | cut -c1-12)
+imageTag=$(echo "${GITHUB_SHA}" | cut -c1-12)
+if [[ "$INPUT_IMAGE_TAG" ]]; then
+	imageTag=$INPUT_IMAGE_TAG
+fi
+
 BASE_NAME="docker.pkg.github.com/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}"
-SHA_NAME="${BASE_NAME}:${shortSHA}"
+SHA_NAME="${BASE_NAME}:${imageTag}"
 
 # Add Arguments For Caching
 BUILDPARAMS=""
@@ -52,6 +55,11 @@ if [ "${INPUT_CACHE}" == "true" ]; then
       BUILDPARAMS=" --cache-from ${BASE_NAME}"
    fi
 fi
+
+if [[ "${INPUT_BUILD_ARGS}" ]]; then
+	BUILDPARAMS="$BUILDPARAMS $INPUT_BUILD_ARGS"
+fi
+
 
 # Build The Container
 docker build $BUILDPARAMS -t ${SHA_NAME} -t ${BASE_NAME} -f ${INPUT_DOCKERFILE_PATH} ${INPUT_BUILD_CONTEXT}
